b9b66f3ffd05c69ea3ee35740218dea460fc157a
iabhinavsinghrajput <abhinavrajput110@gmail.com>
2026-06-22 17:19:42 +0530

    added

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import  { createContext, useEffect, useState } from 'react'
 export const userDataContext=createContext()
 function UserContext({children}) {
-    const serverUrl = "http://localhost:8000";
+    const serverUrl = "https://ai-virtual-assistant-web-application-2btb.onrender.com";
     const [userData,setUserData]=useState(null)
     const [loading,setLoading]=useState(true)
     const [frontendImage,setFrontendImage]=useState(null)
